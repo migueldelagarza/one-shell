305cc4d828521dda4ba02991d806d3518e4ee401
Miguel de la Garza <hola@miguel.delagarza.dev>
2024-11-04 20:50:59 -0600

    update icon to full width

--- projects/app-shell/src/lib/components/toolbar/toolbar.component.ts
+++ projects/app-shell/src/lib/components/toolbar/toolbar.component.ts
@@ -7,22 +7,21 @@ import { MatButtonModule } from "@angular/material/button";
 @Component({
   selector: 'shell-toolbar',
   template: `
-  <mat-toolbar>
     @if (isNotMobile()) {
       <button mat-icon-button (click)="toggle()">
-        <mat-icon>menu</mat-icon>
+        <mat-icon>keyboard_arrow_{{iconText()}}</mat-icon>
       </button>
     }
-  </mat-toolbar>
   `,
   styleUrl: './toolbar.component.css',
   imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   standalone: true
 })
 export class ToolbarComponent {
-  fullWidth = model();
+  readonly fullWidth = model();
   private readonly _viewportService = inject(ViewportService);
   readonly isNotMobile = computed(() => !this._viewportService.isMobile());
+  readonly iconText = computed(() => this.fullWidth() ? 'right' : 'left');
 
   toggle() {
     this.fullWidth.set(!this.fullWidth());
